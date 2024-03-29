79c9079ab6f23d6230b408e9a341444ef7f9c161
Mohit Sharma <ms7820420@gmail.com>
2024-03-29 13:18:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Stopwatch = () => {
+  const [intervalId, setIntervalId] = useState(null);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [intervalId]);
+
+  const startTimer = () => {
+    if (!intervalId) {
+      const id = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 10);
+      setIntervalId(id);
+    }
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+    setTime(0);
+  };
+
+  const formatTime = (value) => {
+    return value < 10 ? "0" + value : value;
+  };
+
+  const millis = formatTime(time % 100);
+  const seconds = formatTime(Math.floor((time % 6000) / 100));
+  const minutes = formatTime(Math.floor((time % 360000) / 6000));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <button className="start" onClick={startTimer}>
+          Start
+        </button>
+        <button className="stop" onClick={stopTimer}>
+          Stop
+        </button>
+        <button className="reset" onClick={resetTimer}>
+          Reset
+        </button>
+      </div>
+      <p>
+        <span>{minutes}</span>:<span>{seconds}</span>:<span>{millis}</span>
+      </p>
     </div>
   );
-}
+};
 
-export default App;
+export default Stopwatch;
